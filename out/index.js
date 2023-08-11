7521045af6eb828271c634cde549a7ae38321fc6
Hendstor <venuefourteen@gmail.com>
2023-08-11 18:51:46 +0800

    removed out

--- out/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.renderedOut = void 0;
-const inPutString = '"This is the output of TypeScript Project"';
-function renderedOut(what = inPutString) {
-    return `outPrinted as : ${what}! `;
-}
-exports.renderedOut = renderedOut;
-console.log(renderedOut());
