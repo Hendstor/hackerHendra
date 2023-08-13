e49ee0d712588bdf293c4f9475971d121095fd03
Hendstor <venuefourteen@gmail.com>
2023-08-13 20:21:52 +0800

    Added Folder Named "practice"
    Add Folder & Store Each Solutions in it

--- index.ts
+++ index.ts
@@ -1,9 +1,29 @@
+import { add } from "./practice/a_veryEasy/a1";
+
 const inPutString = '"This is the output of TypeScript Project"';
 
 export function renderedOut (what: string = inPutString): string {
   return `outPrinted as : ${what}! `;
 }
 
-console.log(renderedOut())
+// console.log(renderedOut())
 
 // comment
+
+// import
+
+const out = add(1, 1)
+console.log(out)
+
+// const testCases = [
+//   [1, 2],
+//   [2, 3],
+//   [10, 20]
+// ]
+
+// for (let i = 0; i < testCases.length; i++) {
+//   const out = add(testCases[i][0], testCases[i][1])
+//   console.log(`testCase: ${JSON.stringify(testCases[i])}, out: ${out}`)
+// }
+// aVeryEasy
+// a_very_easy
---
+++ practice/edabit.ts
@@ -0,0 +1,87 @@
+// // 1st
+// // function hello() {
+// //     return "hello edabit.com"
+// // }
+
+// //2nd
+// // function addition(a, b) {
+// // 	return a + b
+// // }
+
+// //3rd
+// // function howManySeconds(hours) {
+// // 	return hours * 3600
+// // }
+
+// //4th
+// // function giveMeSomething(a) {
+// // 	return "something" + " " + a
+// // }
+
+// //5th
+// // function findPerimeter(length, width) {
+// // 	return (length * 2) + (width * 2)
+// // }
+
+// //6th
+// // function squared(a) {
+// // 	return a * a
+// // }
+
+// //7th
+// // function lessThanOrEqualToZero(num) {
+// // 	if (num <= 0){
+// // 		return true;
+// // 	} else {
+// // 		return false
+// // 	}
+// // }
+
+// //8th
+// // function points(twoPointers, threePointers) {
+// // 	return (twoPointers * 2) + (threePointers * 3) 
+// // }
+
+// //9th
+// function nextEdge(side1, side2) {
+// 	return (side1 + side2)-1
+// }
+
+// // 10th
+// function cubes(a) {
+// 	return (a * a) * a
+// }
+
+// // 11th
+// function addition(num) {
+// 	return num + 1
+// }
+
+// // 12th
+// function swap(a, b) {
+// 	a = a
+// 	b = b
+// 	return [b, a]
+// }
+
+// //13th
+// function printArray(number) {
+//     var newArray = [];
+  
+//     for(var i = 1; i <= number; i++) {
+//       newArray.push(i);
+//     }
+  
+//     return newArray;
+//   }
+
+// //   14th
+// function and(a, b) {
+// 	if(a && b == true){
+// 		return true;
+// 	} else {
+// 		return false
+// 	}
+// }
+
+// console.log(hello())
